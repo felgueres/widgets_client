c32ba2045cca7aab233cdfb8a2a772e09298209c
felgueres <pablofelgueres@gmail.com>
2023-07-19 11:43:31 -0700

    add support for metric system

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -1,4 +1,5 @@
 import { TCurrentWeather } from "@/hooks/useSearch"
+import { useState } from "react"
 
 // TODO: move to utils
 function getDateFromStr(dateStr: string) {
@@ -12,11 +13,8 @@ function getDateFromStr(dateStr: string) {
 
 export default function WeatherWidget({ data }: { data: TCurrentWeather | null }) {
     if (!data) { return null }
-
-    console.log(data)
-
     const { current, location } = data.data
-
+    const [isMetric, setIsMetric] = useState(false)
     return <>
         <div className="text-black border-b pb-6">
             <div className="flex justify-between">
@@ -27,10 +25,19 @@ export default function WeatherWidget({ data }: { data: TCurrentWeather | null }
             <div className="flex justify-between">
                 <div className="flex gap-1 items-center">
                     <img src={current.condition.icon} className="w-16 h-16" />
-                    <div className="text-4xl" > {current.temp_f} °F </div>
+                    <div>
+                        <div className="text-4xl flex gap-1" > 
+                            {isMetric ? current.temp_c : current.temp_f} 
+                            <span className="text-lg align-text-top"> 
+                                <button onClick={() => setIsMetric(true)} className={`${isMetric? '': 'text-gray-300'}`}> °C </button>
+                                <span className="text-gray-200"> | </span>
+                                <button onClick={() => setIsMetric(false)} className={`${isMetric? 'text-gray-300': ''}`}> °F </button>
+                            </span>
+                        </div>
+                    </div>
                     <div className="flex flex-col ml-2">
                         <div className="text-sm" > Humidity: {current.humidity} % </div>
-                        <div className="text-sm" > Wind: {current.wind_mph} mph </div>
+                        <div className="text-sm" > Wind: {isMetric ? `${current.wind_kph} kph` : `${current.wind_mph} mph`} </div>
                     </div>
                 </div>
                 <div className="flex flex-col text-end">
